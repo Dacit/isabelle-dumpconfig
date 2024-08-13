084e452d593941c85dc7c1af56eb24e6d235f33f
Fabian Huch <huch@in.tum.de>
2024-08-13 18:46:28 +0200

    Added setup

---
+++ dump_config.scala
@@ -0,0 +1,14 @@
+/*  Author:     Fabian Huch, TU Muenchen
+
+Dumps Isabelle config.
+*/
+
+package isabelle
+
+
+
+object Dump_Config {
+
+}
+
+class Dump_Config_Tools extends Isabelle_Scala_Tools()
